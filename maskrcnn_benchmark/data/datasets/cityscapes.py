85d2d7a14b2ffc3e11a7f55f1714b9842282b3ae
RuiChen96 <ruichen9618@gmail.com>
2019-08-01 18:15:16 +0800

    cityscapes dataloader almost done

--- maskrcnn_benchmark/data/datasets/cityscapes.py
+++ maskrcnn_benchmark/data/datasets/cityscapes.py
@@ -48,30 +48,57 @@ class Cityscapes(torchvision.datasets.coco.CocoDetection):
         # as you would do normally
         super(Cityscapes, self).__init__(root, ann_file)
 
+        # sort indices for reproducible results
+        # self.ids is inheritance from __init__
+        self.ids = sorted(self.ids)
+
+        self.categories = {cat['id']: cat['name'] for cat in self.coco.cats.values()}
+
         self.id_to_img_map = {k: v for k, v in enumerate(self.ids)}
+
+        self._transforms = transforms
 
     def __getitem__(self, idx):
         # load the image as a PIL Image
-        img, anno = super(Cityscapes, self).__getitem__(idx)
-        # image = ...
+        coco = self.coco
+        img_id = self.ids[idx]
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        anno = coco.loadAnns(ann_ids)
+
+        path = coco.loadImgs(img_id)[0]['file_name']
+        sub_folder = path.split('_')[0]
+        img = Image.open(os.path.join(self.root, sub_folder, path)).convert('RGB')
+
+        # img, anno = super(Cityscapes, self).__getitem__(idx)
+        # anno = target
+
+        # filter crowd annotations
+        anno = [obj for obj in anno if obj["iscrowd"] == 0]
 
         # load the bounding boxes as a list of list of boxes
         # in this case, for illustrative purposes, we use
         # x1, y1, x2, y2 order.
-        boxes = [[0, 0, 10, 10], [10, 20, 50, 50]]
+        boxes = [obj["bbox"] for obj in anno]
+        boxes = torch.as_tensor(boxes).reshape(-1, 4)   # guard against no boxes
+        target = BoxList(boxes, img.size, mode="xywh").convert("xyxy")
+
         # and labels
-        labels = torch.tensor([10, 20])
+        classes = [obj["category_id"] for obj in anno]
+        classes = torch.tensor(classes)
+        target.add_field("labels", classes)
 
         # create a BoxList from the boxes
-        boxlist = BoxList(boxes, image.size, mode="xyxy")
+        # boxlist = BoxList(boxes, image.size, mode="xyxy")
         # add the labels to the boxlist
-        boxlist.add_field("labels", labels)
+        # boxlist.add_field("labels", labels)
 
-        if self.transforms:
-            image, boxlist = self.transforms(image, boxlist)
+        target = target.clip_to_image(remove_empty=True)
+
+        if self._transforms is not None:
+            img, target = self._transforms(img, target)
 
         # return the image, the boxlist and the idx in your dataset
-        return image, boxlist, idx
+        return img, target, idx
 
     def get_img_info(self, index):
         # get img_height and img_width. This is used if
@@ -88,6 +115,13 @@ class Cityscapes(torchvision.datasets.coco.CocoDetection):
 
 if __name__ == '__main__':
     data_dir = '/home/ruichen/data/CityPersons_DATASET/annotations_coco_format'
+    img_dir = '/home/ruichen/data/CityPersons_DATASET/leftImg8bit/train'
     annFile = os.path.join(data_dir, 'instancesonly_filtered_gtFine_train.json')
-    coco = COCO(annFile)
-    print(len(coco))
+    # coco = COCO(annFile)
+    # print(len(coco))
+    data_loader = Cityscapes(ann_file=annFile, root=img_dir, remove_images_without_annotations=False)
+    img, target, idx = data_loader.__getitem__(0)
+    img.show()
+    print('target: {}. '.format(target))
+    print('idx: {}. '.format(idx))
+    print(data_loader.__len__())
