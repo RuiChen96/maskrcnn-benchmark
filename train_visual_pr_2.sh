9912086c2e8b01751282ad183e12324a8f2daf96
RuiChen96 <ruichen9618@gmail.com>
2019-08-10 19:54:18 +0800

    remove green screen data

--- train_visual_pr_2.sh
+++ train_visual_pr_2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export NGPUS=3
-CUDA_VISIBLE_DEVICES=0,2,3 python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/visual_pr/visual_pr_2_e2e_faster_rcnn_R_50_FPN_1x_cocostyle.yaml" MODEL.RPN.FPN_POST_NMS_TOP_N_TRAIN 12000
+CUDA_VISIBLE_DEVICES=0,1,2 python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/visual_pr/visual_pr_2_e2e_faster_rcnn_R_50_FPN_1x_cocostyle.yaml" MODEL.RPN.FPN_POST_NMS_TOP_N_TRAIN 12000
