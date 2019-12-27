f49142daced66d02d536f3da9684cb77910f759b
Rui Chen <ruichen9618@gmail.com>
2019-12-27 15:53:43 +0800

    add training config for 1000skus

---
+++ train_1000skus_res101_4cards.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export NGPUS=4
+/home/rui/anaconda2/envs/maskrcnn/bin/python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/1000skus/pr_e2e_faster_rcnn_R_101_FPN_1x_cocostyle.yaml" MODEL.RPN.FPN_POST_NMS_TOP_N_TRAIN 2000
