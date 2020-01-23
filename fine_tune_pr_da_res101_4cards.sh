1f097092f13d6426809650dcf21bdc64547c12f2
RuiChen96 <ruichen9618@gmail.com>
2020-01-23 11:03:32 -0800

    clean codebase

--- fine_tune_pr_da_res101_4cards.sh
+++ fine_tune_pr_da_res101_4cards.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 export NGPUS=4
 /home/rui/anaconda2/envs/maskrcnn/bin/python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/domain_ad/e2e_faster_rcnn_X_101_32x8d_FPN_1x_200skus.yaml" MODEL.RPN.FPN_POST_NMS_TOP_N_TRAIN 4000
--- inference_without_anno.py
+++ inference_without_anno.py
@@ -2,27 +2,13 @@
 import argparse
 import cv2
 import os
-import torch
-import numpy as np
 from tqdm import tqdm
 
 from maskrcnn_benchmark.config import cfg
 from demo.predictor import COCODemo
-from maskrcnn_benchmark.structures.keypoint import PersonKeypoints
 
-# for COCO annotations
-from pycocotools.coco import COCO
-
-import time
-import math
 import json
-
 import datetime
-import re
-import fnmatch
-from natsort import natsorted
-import random
-import sys
 
 
 ROOT_DIR = '/home/ruichen/Documents/Documents_from_ubuntu_1604/ceiling_camera/'
@@ -31,7 +17,7 @@ ROOT_DIR = '/home/ruichen/Documents/Documents_from_ubuntu_1604/ceiling_camera/'
 # SPLIT_TRAIN_TEST = False
 
 INFO = {
-    "description": "Domain Adaptation",
+    "description": "Domain Adaptation Iteration 1",
     "url": "",
     "version": "0.1.0",
     "year": 2020,
@@ -202,7 +188,7 @@ CATEGORIES = [
 
 
 def main():
-    parser = argparse.ArgumentParser(description="PyTorch Object Detection Webcam Demo")
+    parser = argparse.ArgumentParser(description="Inference on unlabeled nano store images")
     parser.add_argument(
         "--config-file",
         default="configs/caffe2/e2e_mask_rcnn_R_50_FPN_1x_caffe2.yaml",
@@ -265,23 +251,10 @@ def main():
         "images": [],
         "annotations": []
     }
-    coco_output_test = {
-        "info": INFO,
-        "licenses": LICENSES,
-        "categories": CATEGORIES,
-        "images": [],
-        "annotations": []
-    }
 
     image_id = 1
     product_id = 1
-    # no_regions_count = 0
     annotations_train = []
-    # annotations_test = []
-    # missing_annotations = []
-
-    image_names = []
-    # images_processed = []
 
     args = parser.parse_args()
 
@@ -302,153 +275,28 @@ def main():
     # load images from directory
     img_dir = args.img_dir
 
-    # load annotations
-    # annFile = os.path.join(img_dir, '{}.json'.format('annotations_train'))
-    # coco = COCO(annFile)
-
     imgs = os.listdir(img_dir)
-
-    # for img_id in coco.imgs:
-    #     imgs.append((img_id, coco.imgs[img_id], 0))
 
     if len(imgs) == 0:
         print("COULD NOT FIND ANY IMAGE")
     else:
-        print('dataset len of: {}.'.format(len(imgs)))
+        print('Dataset len of: {}.'.format(len(imgs)))
 
-    out_dir = args.out_dir
-
-    himg_count = 1
     pbar = tqdm(total=len(imgs))
     for img in imgs:
         pbar.update(1)
-        start_time = time.time()
 
-        # img_id = img[0]
-        # file_name = img[1]['file_name']
         file_name = img
         img_cv2 = cv2.imread(os.path.join(img_dir, file_name))
 
         composite, predictions = coco_demo.run_on_opencv_image(img_cv2)
-
-        # cv2.imwrite(os.path.join(out_dir, 'test_keypoint.jpg'), composite)
-
-        # print("Time: {:.2f} s / img".format(time.time() - start_time))
-
-        # composite = cv2.resize(composite, None, fx=0.5, fy=0.5)
-
-        # human_labels = predictions.get_field("labels").numpy().tolist()
         product_boxes = predictions.bbox.numpy().tolist()
 
         if len(product_boxes) == 0:
             continue
 
-        # if len(product_boxes) >= 2:
-        #     raise ValueError
-
         product_scores = predictions.extra_fields["scores"].numpy()
         product_labels = predictions.extra_fields["labels"].numpy()
-
-
-
-        # get the keypoints
-        # keypoints = predictions.get_field("keypoints")
-        # kps = keypoints.keypoints
-        # scores = keypoints.get_field("logits")
-        # kps = torch.cat((kps[:, :, 0:2], scores[:, :, None]), dim=2).numpy()
-
-        # print('labels:', human_labels)
-        # print('boxes:', human_boxes)
-        # print('keypoints:', kps.shape, kps)
-
-        # load annotations for each input img
-        # annIds = coco.getAnnIds(imgIds=[img_id], iscrowd=None)
-        # anns = coco.loadAnns(annIds)
-        # classes = []
-        # bboxes = []
-        # for ann in anns:
-        #     cat_id = ann['category_id']
-        #
-        #     if ann['iscrowd']:
-        #         cat_id = -1
-        #     classes.append(cat_id)
-        #     bboxes.append(ann['bbox'])
-        # classes = np.asarray(classes)
-        # bboxes = np.asarray(bboxes)
-        # classes = classes.astype(np.float32)
-        # bboxes = bboxes.astype(np.float32)
-        #
-        # if len(bboxes) == 0:
-        #     continue
-
-        # calculate bboxes center
-        # bboxes_x1 = bboxes[:, 0]
-        # bboxes_y1 = bboxes[:, 1]
-        # bboxes_x2 = bboxes[:, 2]
-        # bboxes_y2 = bboxes[:, 3]
-
-        # bboxes_x_bar = (bboxes_x1 + bboxes_x2) / 2.0
-        # bboxes_y_bar = (bboxes_y1 + bboxes_y2) / 2.0
-        # bboxes_x_bar = (bboxes[0, 0] + bboxes[0, 2]) / 2.0
-        # bboxes_y_bar = (bboxes[0, 1] + bboxes[0, 3]) / 2.0
-
-        # bboxes_centers = np.hstack((bboxes_x_bar, bboxes_y_bar))
-        # bboxes_centers = (bboxes_x_bar, bboxes_y_bar)
-        # print('bboxes_centers: ', bboxes_centers)
-
-        # crop the hands
-        # if kps.shape[0] == 1:
-        # pid means person_id
-        # kps = [1, 2]
-        # for pid in range(kps.shape[0]):
-        #     kps_names = PersonKeypoints.NAMES
-            # h1 denotes left_hand, h2 denotes right_hand
-            # h1xy = kps[pid, kps_names.index('left_wrist'), :2]
-            # h2xy = kps[pid, kps_names.index('right_wrist'), :2]
-
-            # print('h1xy:', h1xy)
-            # print('h2xy:', h2xy)
-
-            # dist = math.sqrt((h1xy[0]-h2xy[0])**2 + (h1xy[1]-h2xy[1])**2)
-            # print('dist between 2 hands:', dist)
-
-            # dist_to_h1 = math.sqrt((bboxes_centers[0] - h1xy[0]) ** 2 + (bboxes_centers[1] - h1xy[1]) ** 2)
-            # dist_to_h2 = math.sqrt((bboxes_centers[0] - h2xy[0]) ** 2 + (bboxes_centers[1] - h2xy[1]) ** 2)
-            # print('dist for product to h1: ', dist_to_h1)
-            # print('dist for product to h2: ', dist_to_h2)
-
-            # hand_images = []
-            # if the hands are separated, crop one image for each hand
-            # img_size = 600
-
-            # if dist_to_h1 > 500.0 and dist_to_h2 > 500.0:
-                # wrong person (grab no products) is detected
-                # continue
-
-            # if dist > 300.0:
-                # if dist_to_h1 < dist_to_h2:
-                # h1, position1 = crop_image(img_cv2, [h1xy[0], h1xy[1]], img_size)
-
-                # new_bboxes = re_calculate_bboxes(bboxes, [h1xy[0], h1xy[1]], img_size, position1)
-
-                # hand_images.append(h1)
-                # else:
-                # h2, position2 = crop_image(img_cv2, [h2xy[0], h2xy[1]], img_size)
-
-                # new_bboxes = re_calculate_bboxes(bboxes, [h2xy[0], h2xy[1]], img_size, position2)
-
-                # hand_images.append(h2)
-            # else:
-                # if both hands are close,
-                # we only need to crop around the middle of them
-                # hmiddle = [(h1xy[0] + h2xy[0])/2, (h1xy[1] + h2xy[1])/2]
-                # h1, position = crop_image(img_cv2, hmiddle, img_size)
-
-                # new_bboxes = re_calculate_bboxes(bboxes, hmiddle, img_size, position)
-
-                # hand_images.append(h1)
-
-            # save the hand images
 
         image_info = {'id': image_id, 'file_name': file_name, 'width': img_cv2.shape[1],
                       'height': img_cv2.shape[0], 'licence': 1, 'coco_url': ""}
@@ -456,24 +304,6 @@ def main():
         coco_output_train["images"].append(image_info)
 
         for idx, pred_bbox in enumerate(product_boxes):
-                # print(new_bboxes)
-
-                # visualization
-                # for i, bbox in enumerate(new_bboxes):
-                #     cv2.rectangle(hand_image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), 0, 1)
-                #     cv2.putText(hand_image, str(classes[i]), (bbox[0], bbox[1] - 8), 1, 1, 2, 1)
-                # visualization
-
-                # cv2.imshow("hand " + str(idx + 1), hand_image)
-                # cv2.waitKey(0)
-                # cv2.imwrite(os.path.join(out_dir, str(himg_count)+".jpg"), hand_image)
-
-                # image_name = str(himg_count) + ".jpg"
-                # image_names.append(image_name)
-
-                # current_img = cv2.imread(os.path.join(out_dir, image_name))
-
-                # insert my code here
             cls = int(product_labels[idx])
             if cls > 30:
                 continue
@@ -481,134 +311,25 @@ def main():
             if score < 0.95:
                 continue
 
-            # print('Training:', file_name)
-
-            # for i, bbox in enumerate(new_bboxes):
             pred_bbox = [int(pos) for pos in pred_bbox]
             x1, y1, x2, y2 = pred_bbox
             w = x2 - x1
             h = y2 - y1
-
-
 
             annotation = {"image_id": image_id, "iscrowd": 0, "area": int(w * h),
                           "bbox": pred_bbox, "segmentation": [],
                           "id": product_id, "category_id": cls, "pred_score": score}
 
             annotations_train.append(annotation)
-
             product_id += 1
-
+        # end-for
         image_id += 1
-
-            # insert my code end here
-
-            # himg_count += 1
-            # image_id += 1
-
-        # cv2.imshow("Detections", composite)
-        # if cv2.waitKey(1) == 27:
-        #     break  # esc to quit
-    # cv2.destroyAllWindows()
-
-    # save coco annotations
+    # end-for
     pbar.close()
     coco_output_train["annotations"] = annotations_train
 
-    with open('{}/annotations_domain_adaptaion_v2.json'.format(ROOT_DIR), 'w') as output_json_file:
+    with open('{}/annotations_domain_adaptaion_iteration_1.json'.format(ROOT_DIR), 'w') as output_json_file:
         json.dump(coco_output_train, output_json_file)
-
-
-def crop_image(image, center, crop_size):
-    imgx1 = int(center[0]-crop_size/2)
-    imgx2 = int(center[0]+crop_size/2)
-    imgy1 = int(center[1]-crop_size/2)
-    imgy2 = int(center[1]+crop_size/2)
-
-    print('image shape:', image.shape)
-    print('x1, x2, y1, y2:', imgx1, imgx2, imgy1, imgy2)
-
-    offx1 = 0
-    offx2 = 0
-    offy1 = 0
-    offy2 = 0
-    if imgx1 < 0:
-        imgx1 = 0
-        offx2 = -(imgx1)
-    if imgx2 >= image.shape[1]:
-        offx1 =  imgx2 - image.shape[1] + 1
-        imgx2 = image.shape[1]-1
-        
-
-    if imgy1 < 0:
-        imgy1 = 0
-        offy2 = -(imgy1)
-    if imgy2 >= image.shape[0]:
-        offy1 =  imgy2 - image.shape[0] + 1
-        imgy2 = image.shape[0]-1
-        
-    
-    print('offx1, offx2, offy1, offy2:', offx1, offx2, offy1, offy2)
-    img_c = image[imgy1-offy1:imgy2+offy2, imgx1-offx1:imgx2+offx2]
-
-    # img + (x1, y1, x2, y2)
-    return img_c, (imgx1-offx1, imgy1-offy1, imgx2+offx2, imgy2+offy2)
-
-
-def re_calculate_bboxes(bboxes, hand_center, crop_size, img_patch):
-    new_bboxes = []
-
-    # top_left = [int(hand_center[0] - (crop_size / 2.0)), int(hand_center[1] - (crop_size / 2.0))]
-    # bottom_right = [int(hand_center[0] + (crop_size / 2.0)), int(hand_center[1] + (crop_size / 2.0))]
-
-    top_left = [img_patch[0], img_patch[1]]
-    bottom_right = [img_patch[2], img_patch[3]]
-
-    for bbox in bboxes:
-        x1, y1, x2, y2 = bbox
-        # x1 -= top_left[0]
-        # y1 -= top_left[1]
-        # x2 -= top_left[0]
-        # y2 -= top_left[1]
-        #
-        # x1 = int(x1)
-        # y1 = int(y1)
-        # x2 = int(x2)
-        # y2 = int(y2)
-
-        if x1 < top_left[0]:
-            x1 = 0
-            # x1 = top_left[0]
-        else:
-            x1 -= (top_left[0] + 1)
-
-        if y1 < top_left[1]:
-            y1 = 0
-            # y1 = top_left[1]
-        else:
-            y1 -= (top_left[1] + 1)
-
-        if x2 > bottom_right[0]:
-            # x2 = bottom_right[0]
-            x2 = bottom_right[0] - (top_left[0] + 1)
-        else:
-            x2 -= (top_left[0] + 1)
-
-        if y2 > bottom_right[1]:
-            y2 = bottom_right[1] - (top_left[1] + 1)
-        else:
-            y2 -= (top_left[1] + 1)
-
-        x1 = int(x1)
-        y1 = int(y1)
-        x2 = int(x2)
-        y2 = int(y2)
-
-        print(x1, y1, x2, y2)
-
-        new_bboxes.append([x1, y1, x2, y2])
-
-    return new_bboxes
 
 
 if __name__ == "__main__":
