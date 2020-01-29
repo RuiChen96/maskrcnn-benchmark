60b3c991d3065efd364224483cce8d3b5f53ba34
RuiChen96 <ruichen9618@gmail.com>
2020-01-29 10:50:41 -0800

    bug fix

--- failure_cases/failure_cases_analysis.sh
+++ failure_cases/failure_cases_analysis.sh
@@ -5,7 +5,7 @@ bbox_dir='/data/rui/syn2real/generated_anno'
 save_dir='/data/rui/syn2real/iter1_vis'
 num_of_skus=1
 
-/home/rui/anaconda2/envs/maskrcnn/bin/python failure_cases.py   --img-dir ${img_dir} \
-                                                                --bbox-dir ${bbox_dir} \
-                                                                --save-dir ${save_dir} \
-                                                                --num ${num_of_skus}
+/home/rui/anaconda2/envs/maskrcnn/bin/python failure_cases_analysis.py  --img-dir ${img_dir} \
+                                                                        --bbox-dir ${bbox_dir} \
+                                                                        --save-dir ${save_dir} \
+                                                                        --num ${num_of_skus}
