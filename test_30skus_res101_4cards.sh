946ba2eb9113addac2d59cd595079e453eee9e44
RuiChen96 <ruichen9618@gmail.com>
2020-01-30 10:44:51 -0800

    bug fix --config

--- test_30skus_res101_4cards.sh
+++ test_30skus_res101_4cards.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export NGPUS=4
-/home/rui/anaconda2/envs/maskrcnn/bin/python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/test_net.py --config-file "configs/1000skus/test_pr_e2e_faster_rcnn_R_101_FPN_1x_cocostyle.yaml"
+/home/rui/anaconda2/envs/maskrcnn/bin/python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/test_net.py --config-file "configs/30skus_syn2real/e2e_faster_rcnn_X_101_32x8d_FPN_1x_synth_blurry.yaml"
