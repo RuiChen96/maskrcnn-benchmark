7af0e4d424d32b1e0177aa2c04510dfab5526233
RuiChen96 <ruichen9618@gmail.com>
2020-03-16 20:43:39 +0800

    add 18skus inference

--- inference_without_anno_photobox.sh
+++ inference_without_anno_photobox.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 config="/home/rui/photobox2real/model_before_adapt/e2e_faster_rcnn_X_101_32x8d_FPN_1x_lightbox.yaml"
-img_dir="/home/rui/real_data_skus_1-30/images"
+img_dir="/home/rui/train_real_pilot1_photobox/images"
 out_dir="/home/rui/photobox2real/generated_anno"
 
 /home/rui/anaconda2/envs/maskrcnn/bin/python inference_without_anno.py  --config-file ${config} \
