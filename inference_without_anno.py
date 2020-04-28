16a2759c36d2426c44ca9a52be2b3bc21fa35722
RuiChen96 <ruichen9618@gmail.com>
2020-04-28 16:47:17 +0800

    update scripts

--- inference_without_anno.py
+++ inference_without_anno.py
@@ -326,7 +326,7 @@ def main():
     pbar.close()
     coco_output_train["annotations"] = annotations_train
 
-    with open('{}/annotations_photobox2real_new_18skus_iter1_0.98.json'.format(out_dir), 'w') as output_json_file:
+    with open('{}/annotations_photobox2real_new_30skus_iter1_0.98.json'.format(out_dir), 'w') as output_json_file:
         json.dump(coco_output_train, output_json_file)
 
 # --config-file
--- inference_without_anno_photobox.sh
+++ inference_without_anno_photobox.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-config="/home/rui/photobox2real/model_before_adapt/e2e_faster_rcnn_X_101_32x8d_FPN_1x_lightbox.yaml"
-img_dir="/home/rui/train_real_pilot1_photobox/images"
+config="/extra/rui/Photobox_new30skus/e2e_faster_rcnn_R_50_FPN_1x_lightbox_new30skus.yaml"
+img_dir="/extra/rui/New30skus/train/train_new30skus_500imgs/body_crop/images"
 out_dir="/home/rui/photobox2real/generated_anno"
 
 /home/rui/anaconda2/envs/maskrcnn/bin/python inference_without_anno.py  --config-file ${config} \
