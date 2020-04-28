d91641bdf5867a8d097625954917e202a5c35d2a
RuiChen96 <ruichen9618@gmail.com>
2020-04-28 18:20:09 +0800

    bug fix

--- train_photobox2real_new_30skus_res50_4cards.sh
+++ train_photobox2real_new_30skus_res50_4cards.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export NGPUS=4
+export NGPUS=2
 /home/rui/anaconda2/envs/maskrcnn/bin/python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/30skus_photobox2real/e2e_faster_rcnn_R_50_FPN_1x_lightbox_new30skus.yaml" MODEL.RPN.FPN_POST_NMS_TOP_N_TRAIN 4000
