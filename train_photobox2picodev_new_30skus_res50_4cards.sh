bc8db4ba384dd5f40036a7daa83e648e7eb61b44
RuiChen96 <ruichen9618@gmail.com>
2020-05-29 10:10:30 +0800

    update .yaml

--- train_photobox2picodev_new_30skus_res50_4cards.sh
+++ train_photobox2picodev_new_30skus_res50_4cards.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export NGPUS=2
+export NGPUS=1
 /home/rui/anaconda2/envs/maskrcnn/bin/python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/photobox2picodev/e2e_faster_rcnn_R_50_pico_dev_r1500_plus_pb.yaml" MODEL.RPN.FPN_POST_NMS_TOP_N_TRAIN 4000
