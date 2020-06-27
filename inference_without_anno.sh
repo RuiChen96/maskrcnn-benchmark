a4672a43637d8b0947203b0ba5d400fd7634d979
RuiChen96 <ruichen9618@gmail.com>
2020-06-27 23:36:06 +0800

    add pseudo-labeling for shanghai data

--- inference_without_anno.sh
+++ inference_without_anno.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-config="configs/photobox2picodev/e2e_faster_rcnn_R_50_pico_dev_r1500_plus_pb.yaml"
-img_dir="/home/rui/test_real_data_pico_dev/images_not_in_testset_crop"
-out_dir="/data/rui/syn2real/generated_anno"
+config="configs/shanghai_skus/e2e_faster_rcnn_R_50_FPN_1x_lightbox.yaml"
+img_dir="/extra/rui/shanghai_store_train_unannotated/cropped"
+out_dir="/home/rui/photobox2real/generated_anno"
 
 /home/rui/anaconda2/envs/maskrcnn/bin/python inference_without_anno.py  --config-file ${config} \
                                                                         --img-dir ${img_dir} \
